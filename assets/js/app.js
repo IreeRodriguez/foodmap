03aadfebf00d87ac116c6f733be3dfd4808e1002
IreeRodriguez <ire208@gmail.com>
2017-12-14 15:23:58 -0300

    mapas con google maps api

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,12 +38,16 @@ $(document).ready(function() {
 					"<div class='modal-content'>"+
 						"<a href='#!' class='right modal-action modal-close waves-effect waves-green btn-flat'><i class='fa fa-times' aria-hidden='true'></i></a>" +
 						"<h4 class='center-align'>" + v.name + "</h4>" +
+                        "<div id='" + counter + "' class= 'video-container'>" +
+                        "</div>" + 
 						"<p><strong>Adress: </strong>" + v.adress + "</p>" +
 						"<p><strong>Price: </strong> <span class='deep-orange-text text-darken-1'>" +
 						v.range + "</span></p>" +
 					"</div>" +
 				"</div>"
 			);
+
+            initMaps(counter,v.adress);
 			//Codigo para inicializar los modals de materialize//
 			$('.modal').modal({
 			  dismissible: true, // Modal can be dismissed by clicking outside of the modal
@@ -62,3 +66,42 @@ $(document).ready(function() {
       $('#splash').fadeOut(500);
   }, 2000);
 });
+
+var map, infoWindow;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6
+});
+
+infoWindow = new google.maps.InfoWindow;
+
+// Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+     // Browser doesn't support Geolocation
+     handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                     'Error: The Geolocation service failed.' :
+                     'Error: Your browser doesn\'t support geolocation.');
+                     infoWindow.open(map);
+}
